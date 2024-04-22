4af0921b0f41ab2b06026aaa687c585778108858
petrostrak <pit.trak@gmail.com>
2024-04-22 16:24:54 +0300

    ADD test case for tcp_transport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -17,4 +18,32 @@ func NewTCPTransport(addr string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: addr,
 	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop () {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %v", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("%+v\n", conn)
 }
