9d87ba4d5b3ad205c08da4422f893f3c5e1ed82f
petrostrak <pit.trak@gmail.com>
2024-05-19 20:38:47 +0300

    DELETE & for decoder

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	opts := p2p.TCPTransportOps{
 		ListenAddr: ":3000",
 		ShakeHands: p2p.NoHandshake,
-		Decoder:    &p2p.DefaultDecoder{},
+		Decoder:    p2p.DefaultDecoder{},
 	}
 
 	tr := p2p.NewTCPTransport(opts)
